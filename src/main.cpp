7df38a6eaa6f65549a6ecd98ef4ebbcb2860c6cd
ethanguin <ethanielbishop@gmail.com>
2025-02-06 19:32:55 -0700

    add(main) file versioning

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include <list>
 #include <string>
 #include "pixel.h"
 #include "Raytracer.h"
 
+bool is_file_exist(std::string fileName) {
+    std::ifstream infile(fileName);
+    return infile.good();
+}
+
 void exportImage(const std::list<pixel> &pixel_values, int width, int height) {
-    std::ofstream fileOut("output.ppm");
+    std::string filename = "Raytrace_output0.ppm";
+    while (is_file_exist(filename)) {
+        int num = 1;
+        filename = "Raytrace_output" + std::to_string(num) + ".ppm";
+    }
+    std::ofstream fileOut(filename);
     if (!fileOut) {
         std::cerr << "Error: Could not open output.ppm for writing." << std::endl;
         return;
